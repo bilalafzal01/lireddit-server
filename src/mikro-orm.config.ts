969270753f048b8d7245437c48c09939cbf6729c
Bilal Afzal <bilal@shiftcrowd.co.uk>
2021-07-04 15:54:25 +0500

    Added User Entity + Migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 export default {
   dbName: "lireddit",
@@ -9,7 +10,7 @@ export default {
   password: "password",
   type: "postgresql",
   debug: !__prod__,
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
